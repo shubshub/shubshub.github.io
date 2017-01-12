ac610dfe1d4177da82b6dfad9ded578ca51496f8
shubshub <cynthshub@gmail.com>
2017-01-13 11:46:48 +1300

    Nintendo Switch Bingo!

---
+++ SwitchBingo/main.js
@@ -0,0 +1,138 @@
+var image_order = [
+	"zelda_exclusive",
+	"nintendo_eshop",
+	"mario_kart",
+	"miiverse",
+	"achievements",
+	"multi_touch_screen",
+	"smash_bros",
+	"new_ip",
+	"battery_life",
+	"subscription_service",
+	"joycon_pointer",
+	"mario_game_3d",
+	"ram_amount",
+	"no_region_lock",
+	"analog_triggers",
+	"launch_price",
+	"usb_c_charging",
+	"joycon_sold_seperately",
+	"share_button",
+	"mobile_game_emulator",
+	"no_discs",
+	"ar_compatibility",
+	"virtual_console",
+	"backwards_compatible_digital"
+]
+
+var startingLeft = 6;
+var horizontalVerticalSpacing = 6;
+var endingRight = 5;
+var panelsPlaced = 0;
+
+var globalX = 6;
+var globalY = 89;
+var imageArray = [];
+var activeImageArray = [];
+var baseLayer;
+
+var xLocation = [];
+var yLocation = [];
+
+var imageType = [];
+
+
+function init()
+{
+	baseLayer = document.createElement("img");
+	baseLayer.src = "layout.png";
+	for (var i = 0; i < image_order.length; i++)
+	{
+		imageArray[i] = document.createElement("img");
+		imageArray[i].src = "panels/" + image_order[i] + ".png";
+		
+		activeImageArray[i] = document.createElement("img");
+		activeImageArray[i].src = "active/" + image_order[i] + ".png";
+	}
+}
+
+init();
+
+function clickHandler(event)
+{
+	var canvas = document.getElementById("bingoPanel");
+	var context = canvas.getContext("2d");
+	
+	for (var i = 0; i < image_order.length; i++)
+	{
+		if (event.clientX > xLocation[i] && event.clientX < xLocation[i] + imageArray[i].width)
+		{
+			if (event.clientY > yLocation[i] && event.clientY < yLocation[i] + imageArray[i].height)
+			{
+				if(imageType[i] == 0)
+				{
+					context.drawImage(activeImageArray[i], xLocation[i], yLocation[i]);
+					imageType[i] = 1;
+				}
+				else if (imageType[i] == 1)
+				{
+					context.drawImage(imageArray[i], xLocation[i], yLocation[i]);
+					imageType[i] = 0;
+				}
+			}
+		}
+		
+		
+	}
+}
+
+function placeAllPanels()
+{
+	var canvas = document.getElementById("bingoPanel");
+	canvas.addEventListener("click", function(event)
+	{
+		clickHandler(event);
+	});
+	var context = canvas.getContext("2d");
+	
+	$(canvas).ready(function(){
+		context.drawImage(baseLayer, 0, 0);
+		
+		for (var i = 0; i < image_order.length; i++)
+		{
+			
+
+			context.drawImage(imageArray[i], globalX, globalY);
+			xLocation[i] = globalX;
+			yLocation[i] = globalY;
+			imageType[i] = 0;
+			panelsPlaced+=1;
+			y = i+1;
+			if (panelsPlaced == 24)
+			{
+				y = i;
+			}
+			
+			if (panelsPlaced == 5 || panelsPlaced == 10 || panelsPlaced == 14 || panelsPlaced == 19)
+			{
+				//Change to new Row
+				globalX = 6;
+				globalY += imageArray[y].height + 6;
+			}
+			else if (panelsPlaced == 12)
+			{
+				globalX += (imageArray[y].width +6 ) + 153 + 6; //Skip the Freespace
+			}
+			else
+			{
+				globalX += imageArray[y].width + 6;
+			}
+		}
+		
+		console.log("Done");
+	});
+	
+	
+	
+	
+}
