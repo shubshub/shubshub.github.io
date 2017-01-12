cb6ea401f4883efa433cc3f66c869aca487171d2
shubshub <cynthshub@gmail.com>
2017-01-13 12:04:35 +1300

    Randomization Thing

--- SwitchBingo/main.js
+++ SwitchBingo/main.js
@@ -41,6 +41,27 @@ var yLocation = [];
 
 var imageType = [];
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+image_order = shuffle(image_order);
+
 
 function init()
 {
